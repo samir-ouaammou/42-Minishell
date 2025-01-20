19e73921bddc8d1d006c6b1f1c720cf33b73af2d
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-01-20 23:06:31 +0100

    1337 bg

--- parsing/ft_input_validator.c
+++ parsing/ft_input_validator.c
@@ -9,7 +9,11 @@ void	ft_check_the_brackets(t_parsing *shell)
 	while (shell->cmds_split[shell->i])
 	{
 		shell->j = 0;
-		while (shell->cmds_split[shell->i][shell->j])
+		while (shell->cmds_split[shell->i][shell->j] && shell->cmds_split
+			[shell->i][0] != 34 && shell->cmds_split[shell->i][0] != 39
+			&& shell->cmds_split[shell->i][ft_strlen(shell->cmds_split
+				[shell->i]) - 1] != 34 && shell->cmds_split[shell->i]
+				[ft_strlen(shell->cmds_split[shell->i]) - 1] != 39)
 		{
 			if (shell->cmds_split[shell->i][shell->j] == '(')
 				shell->parentheses++;
@@ -24,12 +28,12 @@ void	ft_check_the_brackets(t_parsing *shell)
 		}
 		shell->i++;
 	}
-	if (shell->parentheses != 0)
-		ft_free_all(shell);
 }
 
 void	ft_check_syntax_errors(t_parsing *shell)
 {
 	ft_check_the_brackets(shell);
+	if (shell->parentheses != 0)
+		ft_free_all(shell);
 	ft_check_operator_position(shell);
 }
