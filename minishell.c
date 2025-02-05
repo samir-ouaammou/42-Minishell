47208c1139110d0de6c27dfd45fac143e2d1da2f
7NL <131011542+ahaded777@users.noreply.github.com>
2025-02-05 15:08:41 +0100

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -1,20 +1,21 @@
 #include "parsing/Minishell.h"
 #include "exaction/exaction.h"
 
-int	main(int ac, char **av, char **env)
+int main(int ac, char **av, char **env)
 {
-	t_parsing	shell;
-    t_data data;
+	t_parsing shell;
+	t_data data;
 
 	if (ac != 1)
 	{
 		write(2, "Error: Invalid number of arguments.\n", 36);
 		exit(-1);
 	}
+	memset(&data, 0, sizeof(t_data));
+	data.name_pro = "➜ Minishell ";
 	while (1)
 	{
-		memset(&data, 0, sizeof(t_data));
-		shell.input = readline("➜ Minishell ");
+		shell.input = readline(data.name_pro);
 		ft_parsing(&shell);
 		if (shell.free == -1 && (!shell.tokens || !shell.three))
 			write(2, "minishell: syntax error\n", 24);
