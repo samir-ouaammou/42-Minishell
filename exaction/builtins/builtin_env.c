a154cc3aeb9b6b0733ee2484222743fa36da6a69
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2025-02-10 12:24:22 +0100

    Update builtin_env.c

--- exaction/builtins/builtin_env.c
+++ exaction/builtins/builtin_env.c
@@ -2,19 +2,39 @@
 
 int builtin_env(char **args, t_data *data)
 {
-    if (!data->env)
+    int i;
+
+    if (!data || !data->env || !args)
         return (-1);
-    if (args[1])
+    else if (args && args[1])
+        write(2, "minishell: env: too many arguments\n", 35);
+    else
     {
-        printf("Error\n");
-        return (-1);
-    }
-    int i = 0;
-    while (data->env[i])
-    {
-        ft_putstr(data->env[i]);
-        ft_putchar('\n');
-        i++;
+        i = 0;
+        while (data->env[i])
+        {
+            ft_printf("%s\n", data->env[i]);
+            i++;
+        }
     }
     return (0);
 }
+
+// int builtin_env(char **args, t_data *data)
+// {
+//     if (!data->env)
+//         return (-1);
+//     if (args[1])
+//     {
+//         printf("Error\n");
+//         return (-1);
+//     }
+//     int i = 0;
+//     while (data->env[i])
+//     {
+//         ft_putstr(data->env[i]);
+//         ft_putchar('\n');
+//         i++;
+//     }
+//     return (0);
+// }
