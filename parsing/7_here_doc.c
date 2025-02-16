530ba222f2b1f42417f4f5545376e206c4b8798f
Samir Ouaammou <souaammo@c3r10p10.1337.ma>
2025-02-16 11:14:27 +0100

    update all

--- parsing/7_here_doc.c
+++ parsing/7_here_doc.c
@@ -2,116 +2,56 @@
 
 int ft_is(char c)
 {
-    if (c == '<' || c == '>' || c == '|' || c == '&' || c == ' ' || c == '\t')
+    if (c == '<' || c == '>' || c == '|' || c == '&')
         return (1);
     return (0);
 }
 
-void ft_here_doc(t_parsing *shell)
+void ft_here_doc(t_parsing *shell, char *str)
 {
-    char *delimiter;
-    char *line;
-    int start;
-    char quote;
-    int j;
+    char    *line;
+    char    *temp;
+    char    *help;
+    char    c;
+    int     i;
+    int     j;
 
-    if (!shell || !shell->input)
-        return;
+    if (!str || !str[0])
+        return ;
 
-    shell->i = 0;
-    while (shell->input[shell->i])
+    i = 0;
+    while (str[i])
     {
-        if (shell->input[shell->i] == '"' || shell->input[shell->i] == '\'')
+        if (str[i] == 34 || str[i] == 39)
         {
-            quote = shell->input[shell->i];
-            start = shell->i;
-            shell->i++;
-            while (shell->input[shell->i] && shell->input[shell->i] != quote)
-                shell->i++;
-            if (shell->input[shell->i] == '\0')
-                return;
-            shell->i++;
+            c = str[i++];
+            while (str[i] && str[i] != c)
+                i++;
+            if (str[i])
+                i++;
         }
-
-        if (shell->input[shell->i] == '<' && shell->input[shell->i + 1] == '<')
+        else if (str[i] == '<' && str[i + 1] == '<')
         {
-            shell->i += 2;
-            j = shell->i;
-
-            while (shell->input[shell->i] == ' ' || shell->input[shell->i] == '\t')
-                shell->i++;
-
-            if (shell->input[shell->i] == '<')
+            while (str[i] && str[i] == ' ' || str[i] == '\n')
+                i++;
+            if (ft_is(str[i]))
+                ft_free_args(shell);
+            j = i;
+            while (str[j] && !ft_is(str[j]))
             {
-                write(1, "error\n", 6);
-                return;
-            }
-
-            if (shell->input[shell->i] == '\0')
-                return;
-
-            if (shell->input[shell->i] == '"' || shell->input[shell->i] == '\'')
-            {
-                quote = shell->input[shell->i];
-                start = shell->i;
-                shell->i++;
-                while (shell->input[shell->i] && shell->input[shell->i] != quote)
-                    shell->i++;
-
-                if (shell->input[shell->i] == '\0')
+                if (str[j] == 34 || str[j] == 39)
                 {
-                    while (1)
-                    {
-                        line = readline("here_doc> ");
-                        if (!line)
-                            break;
-                        free(line);
-                    }
-                    write(1, "\n", 1);
-                    return;
+                    c = str[j++];
+                    while (str[j] && str[j] != c)
+                        j++;
                 }
-                shell->i++;
-            }
-            else
-            {
-                start = shell->i;
-                while (shell->input[shell->i] && shell->input[shell->i] != '\n' && shell->input[shell->i] != '"' && shell->input[shell->i] != '\'' && !ft_is(shell->input[shell->i]))
-                    shell->i++;
-            }
-
-            if (shell->input[shell->i] == '\n')
-            {
-                write(1, "error\n", 6);
-                return;
-            }
-
-            if (start != shell->i)
-            {
-                delimiter = strndup(shell->input + start, shell->i - start);
-                if (!delimiter)
-                    return;
-
-                while (1)
-                {
-                    line = readline("here_doc> ");
-                    if (!line)
-                        break;
-                    if (line[strlen(line) - 1] == '\n')
-                        line[strlen(line) - 1] = '\0';
-                    if (strcmp(line, delimiter) == 0)
-                    {
-                        free(line);
-                        break;
-                    }
-                    free(line);
-                }
-                free(delimiter);
+                if (str[j])
+                    j++;
             }
         }
         else
-        {
-            shell->i++;
-        }
+            i++;
     }
+
 }
 
