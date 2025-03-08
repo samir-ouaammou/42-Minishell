00436e75a105f01f111979325652f98a18f486b6
Samir-Ouaammou <samirouaammou@gmail.com>
2025-03-08 02:37:26 +0000

    norminette

--- wildcards/wildcard.c
+++ wildcards/wildcard.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souaammo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 02:36:12 by souaammo          #+#    #+#             */
+/*   Updated: 2025/03/08 02:36:16 by souaammo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 char	*ft_compress_wildcards(const char *input)
@@ -28,17 +40,37 @@ char	*ft_compress_wildcards(const char *input)
 	return (res);
 }
 
-int	is_wildcard_match(const char *wildcard, const char *filename)
+int	wildcard_match(char *wildcard, char *filename, ssize_t *j)
 {
-	int	i;
-	int	j;
-	int	star_i;
-	int	star_j;
+	ssize_t(i) = 0, (star_i) = -1, (star_j) = -1;
+	while (filename[(*j)] != '\0')
+	{
+		if (wildcard[i] == '*')
+		{
+			star_i = i;
+			star_j = (*j);
+			i++;
+		}
+		else if (wildcard[i] == filename[(*j)])
+		{
+			i++;
+			(*j)++;
+		}
+		else if (star_i != -1)
+		{
+			i = star_i + 1;
+			star_j++;
+			(*j) = star_j;
+		}
+		else
+			return (-100);
+	}
+	return (i);
+}
 
-	i = 0;
-	j = 0;
-	star_i = -1;
-	star_j = -1;
+int	is_wildcard_match(char *wildcard, char *filename)
+{
+	ssize_t(i) = 0, (j) = 0;
 	if (!wildcard || !wildcard[0] || !filename || !filename[0])
 		return (0);
 	wildcard = ft_compress_wildcards(wildcard);
@@ -50,28 +82,9 @@ int	is_wildcard_match(const char *wildcard, const char *filename)
 		return (filename[0] == '.');
 	if (ft_strcmp(wildcard, "*.") == 0)
 		return (0);
-	while (filename[j] != '\0')
-	{
-		if (wildcard[i] == '*')
-		{
-			star_i = i;
-			star_j = j;
-			i++;
-		}
-		else if (wildcard[i] == filename[j])
-		{
-			i++;
-			j++;
-		}
-		else if (star_i != -1)
-		{
-			i = star_i + 1;
-			star_j++;
-			j = star_j;
-		}
-		else
-			return (0);
-	}
+	i = wildcard_match(wildcard, filename, &j);
+	if (i == -100)
+		return (0);
 	if (wildcard[i] == '*')
 		i++;
 	return (wildcard[i] == '\0' && filename[j] == '\0' && (wildcard[0] == '.'
